18098d9386bc11e2611f485807ca96e918cbb9a1
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-30 15:50:31 +0700

    comit

--- linkedqueue.cpp
+++ linkedqueue.cpp
@@ -92,7 +92,36 @@ int main() {
             cout << "1. Implement insert operation"<< endl;
             cout << "2. Implement delete operation"<< endl;
             cout << "3. Display values"<< endl;
+            cout << "4. Exit "<< endl;
+            cout << "Enter your choice(1-4): ";
+            cin >> ch;
+            cout << endl;
 
+            
+                        switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!" << endl;
+				break;
+			}
+			}
         }
-    }
+	}
+
 }
+			
+		
